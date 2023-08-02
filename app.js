858a133efa447ec7681e3980a14804440e6382d2
PIDUGU Harish <harish.pidugu@hexagon.com>
2023-08-02 18:38:01 +0530

    Added Assignments

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-
 /* ReactElement is object*/
 
-const parent = React.createElement("div", {id: "parent"},
-[React.createElement("div", {id: "child1"},[React.createElement("h1", {},"I am H1 Tag"),React.createElement("h2", {},"I am H2 Tag")]),
- React.createElement("div", {id: "child2"},[React.createElement("h3", {},"I am H3 Tag"),React.createElement("h6", {},"I am H4 Tag")])]);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-console.log(parent)
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I am Java Script Expert :)"),
+    React.createElement("h2", {}, "I am H2 Tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h3", {}, "I am H3 Tag"),
+    React.createElement("h6", {}, "I am H4 Tag"),
+  ]),
+]);
+
+console.log(parent);
 
 //JSX
 
 //CROSS ORIGIN
 
-const heading = React.createElement("h1",{id:"heading", xyz: "abc"}, "Hello World From React");
+const heading = React.createElement(
+  "h1",
+  { id: "heading", xyz: "abc" },
+  "Hello World From React"
+);
 const root = ReactDOM.createRoot(document.getElementById("hello"));
 root.render(parent);
-
